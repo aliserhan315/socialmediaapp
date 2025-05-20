fce24b24f4fd4cab90eb98e988aee26cd3d343b6
LoulouHallal <loulou.hallal7@gmail.com>
2025-05-20 16:04:27 +0300

    story display edit

--- app/(app)/stories/page.jsx
+++ app/(app)/stories/page.jsx
@@ -1,11 +1,21 @@
+import Box from "@/components/Box";
 import Stories from "@/components/Stories";
+import css from "@/styles/PostGenerator.module.css";
 
 export default function StoriesPage() {
   return (
     <div className="p-4">
-      <h1 className="text-xl font-bold mb-4 text-white bg-blue-600 p-4 rounded-t-lg">
-        Stories
-      </h1>
+      <Box className={css.container}>
+        <h1
+          className="text-xl font-bold mb-14 bg-blue-600 p-4 rounded-t-lg bg-primary "
+          style={{ color: "#0070f3" }}
+        >
+          Stories
+        </h1>
+      </Box>
+      <div>
+        <br />
+      </div>
       <Stories />
     </div>
   );
